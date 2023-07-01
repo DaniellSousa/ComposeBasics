e6542abbbb9753eb2a36cb0ca17b31f0e792f2c3
daniel_monteiro_ <daniell.monteirosousa@gmail.com>
2023-07-01 00:38:04 -0300

    A little bit Material Design.. - Use Material Design; - Color; - Typography; - Shape; - Enable dark theme;

--- app/src/main/java/com/example/composebasics/layouts/Layouts.kt
+++ app/src/main/java/com/example/composebasics/layouts/Layouts.kt
@@ -1,8 +1,11 @@
 package com.example.composebasics.layouts
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
@@ -22,14 +25,27 @@ fun MessageCard(msg: Message) {
             modifier = Modifier
                 .size(40.dp)
                 .clip(CircleShape)
+                .border(1.5.dp, MaterialTheme.colors.secondary, CircleShape)
         )
         
         Spacer(modifier = Modifier.width(8.dp))
 
         Column {
-            Text(text = msg.author)
+            Text(
+                text = msg.author,
+                color = MaterialTheme.colors.secondaryVariant,
+                style = MaterialTheme.typography.subtitle2
+            )
+
             Spacer(modifier = Modifier.height(4.dp))
-            Text(text = msg.body)
+
+            Surface(shape = MaterialTheme.shapes.medium, elevation = 1.dp) {
+                Text(
+                    text = msg.body,
+                    modifier = Modifier.padding(all = 4.dp),
+                    style = MaterialTheme.typography.body2
+                )
+            }
         }
     }
     
---
+++ app/src/main/java/com/example/composebasics/material_design/MaterialDesignActivity.kt
@@ -0,0 +1,43 @@
+package com.example.composebasics.material_design
+
+import android.content.res.Configuration
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.composebasics.layouts.Message
+import com.example.composebasics.layouts.MessageCard
+
+class MaterialDesignActivity: ComponentActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            MaterialTheme {
+                Surface(modifier = Modifier.fillMaxSize()) {
+                    MessageCard(msg = Message("Android", "Jetpak Compose"))
+                }
+            }
+        }
+    }
+
+//    @Preview(name = "Light Mode")
+    @Preview(
+        uiMode = Configuration.UI_MODE_NIGHT_YES,
+        showBackground = true,
+        name = "Dark Mode"
+    )
+    @Composable
+    fun PreviewMessageCard() {
+        MaterialTheme {
+            Surface {
+                MessageCard(msg = Message("Danielzin", "Se liga no compose"))
+            }
+        }
+    }
+}
